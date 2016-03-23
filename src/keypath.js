87cb011b3538df1b42ffc784849321d65fff06a6
bydooweedoo <bydooweedoo@gmail.com>
2016-03-23 16:54:55 +0800

    Fix documentation

--- src/keypath.js
+++ src/keypath.js
@@ -6,8 +6,9 @@ import R from 'ramda';
  *      const simplePath = createKeyPathAt('a');
  *      const nestedPath = createKeyPathAt('a.b.c');
  *
- *      setToKeyPath(simplePath, 'OK', {}); //=> {a: 'OK'}
- *      setToKeyPath(nestedPath, 'OK', {}); //=> {a: {b: {c: 'OK'}}}
+ *      R.set(simplePath, 'OK', {}); //=> {a: 'OK'}
+ *      R.set(nestedPath, 'OK', {}); //=> {a: {b: {c: 'OK'}}}
+ *      R.set(simplePath, 'updated', {a: true, b: false}); //=> {a: 'updated', b: false}
  */
 export const createKeyPathAt = R.compose(R.lensPath, R.split('.'));
 
@@ -15,9 +16,7 @@ export const createKeyPathAt = R.compose(R.lensPath, R.split('.'));
  * Set given value at given path for given object.
  * If value already exists for given path, override it.
  *
- *      const path = createKeyPathAt('a.b.c');
- *
- *      setToKeyPath(path, 'OK', {}); //=> {a: {b: {c: 'OK'}}}
+ *      setToKeyPath('a.b.c', 'OK', {}); //=> {a: {b: {c: 'OK'}}}
  */
 export const setToKeyPath = R.curry(
     (path, value, obj) => R.set(createKeyPathAt(path), value, obj)
@@ -27,10 +26,8 @@ export const setToKeyPath = R.curry(
  * Get value from given object at given path.
  * Returns undefined if path does not exists.
  *
- *      const path = createKeyPathAt('a.b.c');
- *
- *      getFromKeyPath(path, {a: {b: {c: 'OK'}}}); //=> 'OK'
- *      getFromKeyPath(path, {a: true}); //=> undefined
+ *      getFromKeyPath('a.b.c', {a: {b: {c: 'OK'}}}); //=> 'OK'
+ *      getFromKeyPath('a.b.c', {a: true}); //=> undefined
  */
 export const getFromKeyPath = R.curry(
     (path, obj) => R.view(createKeyPathAt(path), obj)
